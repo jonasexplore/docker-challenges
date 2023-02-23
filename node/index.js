26d1951bfeb4aa5759262171a8fe34cdc75f92bf
Jonas <jonasexplore@gmail.com>
2023-02-23 20:24:26 -0300

    feat: add mysql insert

--- node/index.js
+++ node/index.js
@@ -1,10 +1,32 @@
 const express = require("express");
+const mysql = require("mysql");
 const cors = require("cors");
 const app = express();
+
+const config = {
+  host: "db",
+  user: "root",
+  password: "root",
+  database: "nodedb",
+};
 
 app.use(cors());
 
 app.get("/", (req, res) => {
+  const connection = mysql.createConnection(config);
+  const sql = `
+    CREATE TABLE IF NOT EXISTS 
+      people (
+        id int not null auto_increment, 
+        name varchar(255), 
+        primary key(id)
+      ); 
+      
+    INSERT INTO people(name) values('Full Cycle')`;
+
+  connection.query(sql);
+  connection.end();
+
   return res.send("<h1>Full Cycle Rocks!</h1>");
 });
 
